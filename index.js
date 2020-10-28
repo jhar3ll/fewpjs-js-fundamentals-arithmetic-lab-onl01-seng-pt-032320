1891a5b7445ed5c64c9a882e2016065a203dccf7
jhar3ll <jharell.chambers@selu.edu>
2020-10-28 18:11:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+let num1 = Math.floor(Math.random() * 10) + 1;
+let num2 = Math.floor(Math.random() * 10) + 1;
+let overFour = Math.ceil(Math.random() * 10) + 4;
+
+let multiply = ((num1 * num2) * 0) + 62;
+
+let random = Math.ceil(Math.random() * 10) + 1;
+
+let mod = (4 % overFour);
+
+let max = Math.max(20)
